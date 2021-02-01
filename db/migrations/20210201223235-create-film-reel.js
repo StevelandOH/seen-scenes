b1da37452c86c35f59f80c6351d437571f08a338
Stephen Szelpal <stephenszelpal@icloud.com>
2021-02-01 18:19:20 -0500

    created and linked the session secret

--- db/migrations/20210201223235-create-film-reel.js
+++ db/migrations/20210201223235-create-film-reel.js
@@ -1,32 +1,32 @@
 'use strict';
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('FilmReels', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      filmId: {
-        type: Sequelize.INTEGER,
-        references: {model: 'Films'}
-      },
-      reelId: {
-         type: Sequelize.INTEGER,
-         references: {model: 'Reels'}
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
-  },
-  down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('FilmReels');
-  }
+	up: (queryInterface, Sequelize) => {
+		return queryInterface.createTable('FilmReels', {
+			id: {
+				allowNull: false,
+				autoIncrement: true,
+				primaryKey: true,
+				type: Sequelize.INTEGER,
+			},
+			filmId: {
+				type: Sequelize.INTEGER,
+				references: { model: 'Films' },
+			},
+			reelId: {
+				type: Sequelize.INTEGER,
+				references: { model: 'Reels' },
+			},
+			createdAt: {
+				allowNull: false,
+				type: Sequelize.DATE,
+			},
+			updatedAt: {
+				allowNull: false,
+				type: Sequelize.DATE,
+			},
+		});
+	},
+	down: (queryInterface, Sequelize) => {
+		return queryInterface.dropTable('FilmReels');
+	},
 };
